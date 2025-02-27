3ae402054191593be4511d30f128f472e4d1d103
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-02-27 16:21:12 +0600

    production build

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,6 +5,7 @@ export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: fetchBaseQuery({ 
         baseUrl: 'https://bi-cycle-server-five.vercel.app/api',
+        //  baseUrl: 'http://localhost:5000/api',
         credentials : 'include'
          }),
 
